2e8d4b3dd246a37df2358e4b339fb5482a5189fd
Jovan Lloyd Honrada <94544981+mrnewbi3@users.noreply.github.com>
2024-12-08 22:16:42 +0800

    fixed hamburger button

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+
+}
 // Function to enable dark mode
 const enableDarkmode = () => {
     document.body.classList.add("darkmode");
